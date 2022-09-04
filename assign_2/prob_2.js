e84f7ebdc47e23f8ead72f9bb5445bbf487c922c
Aasim Patel <aasimpatel97@gmail.com>
2022-09-04 18:53:54 +0530

    Second Commit

---
+++ assign_2/prob_2.js
@@ -0,0 +1,3 @@
+let one=1, two=2, three=3, four=4, five=5;
+
+console.log((2*one)+(2*two)+(2*three)+(2*four)+(2*five));
--- problem_3.js
+++
@@ -1,4 +0,0 @@
-var name = "Asif";
-var age = 23;
-console.log(name, age);
-console.log(typeof (name), typeof (age));
