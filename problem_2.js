8bb04170ce207a6b83d1a439c5e68a3777bae148
Aasim Patel <aasimpatel97@gmail.com>
2022-09-04 17:33:43 +0530

    first commit for JS101

---
+++ problem_2.js
@@ -0,0 +1,6 @@
+var name = "Asif";
+console.log(name);
+name = "Iqbal";
+console.log(name);
+name = "Dilshad";
+console.log(name);
---
+++ problem_3.js
@@ -0,0 +1,4 @@
+var name = "Asif";
+var age = 23;
+console.log(name, age);
+console.log(typeof (name), typeof (age));
